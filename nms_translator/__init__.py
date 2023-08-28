6a9ce655a477e4106ea17e6390f537f040d792e3
Dakota Brown <dakota.kae.brown@gmail.com>
2023-08-28 12:55:59 -0600

    Update minor version

--- nms_translator/__init__.py
+++ nms_translator/__init__.py
@@ -1,11 +1,1 @@
-import toml
-from pathlib import Path
-
-
-def get_version():
-    path = Path(__file__).resolve().parents[1] / "pyproject.toml"
-    pyproject = toml.loads(open(str(path)).read())
-    return pyproject["tool"]["poetry"]["version"]
-
-
-__version__ = get_version()
+__version__ = "0.2.0"
